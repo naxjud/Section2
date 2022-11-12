047e39eeaa4506272281105c413277659491f515
Zraidi <m.zraidi@lenhart-gruppe.de>
2022-11-12 21:47:56 +0100

    PDO Connection to MySQL-DB

--- index.php
+++ index.php
@@ -1,4 +1,22 @@
 <?php
 require "helpers.php";
 
-require "router.php";
+// require "router.php";
+
+//connect to our MySQL database.
+ 
+$dns="mysql:host=localhost;port=3306;dbname=myapp;user=root;charset=utf8mb4";
+
+$pdo = new PDO($dns);
+
+$statement = $pdo->prepare("select * from posts");
+
+$statement->execute();
+
+$posts = $statement->fetchAll(PDO::FETCH_ASSOC);
+
+
+foreach ($posts as $post) {
+    # code...
+    echo "<li>".$post['title']."</li>";
+}
